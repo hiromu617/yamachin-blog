6d46016165f4d09287aeddc4015c60b19529db1a
hiro <hiromhiro617@gmail.com>
2021-09-08 23:03:07 +0900

    :tada: setup

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+import { client } from "../../libs/client";
+
+type Props = {
+  blog: any;
+};
+
+const BlogId: NextPage<Props> = ({ blog }) => {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+  );
+};
+export default BlogId;
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data: any = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content: any) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
