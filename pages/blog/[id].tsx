ee6a52e432040f53eddfb512267777bb107e1919
hiro <hiromhiro617@gmail.com>
2021-09-10 11:19:10 +0900

    :sparkles: add thmbnail

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from 'next/image'
 import { client } from "../../libs/client";
 import { format } from "date-fns";
 import { Blog } from "../../src/types/Blog";
@@ -12,6 +13,7 @@ const BlogId: NextPage<Props> = ({ blog }) => {
   return (
     <main className="container md:px-10 mb-16">
       <div className="text-center mb-5 md:mb-16">
+        {blog.thumbnail && <Image width={+blog.thumbnail.width} height={+blog.thumbnail.height} src={blog.thumbnail.url} alt={"thumbnail"} />}
         <h1 className="text-lg md:text-2xl text-gray-800 my-2 md:my-5 font-semibold">
           {blog.title}
         </h1>
@@ -33,7 +35,7 @@ const BlogId: NextPage<Props> = ({ blog }) => {
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
-        className="prose text-gray-700 mx-auto"
+        className="prose-yellow prose-sm md:prose text-gray-700 mx-auto"
       />
     </main>
   );
--- src/types/Blog.ts
+++ src/types/Blog.ts
@@ -4,8 +4,13 @@ export type Blog = {
   readonly id: string;
   readonly title: string;
   readonly body: string;
-  readonly tags: Tag[]
+  readonly tags: Tag[];
   readonly createdAt: string;
   readonly publishedAt: string;
   readonly reviseAt: string;
+  readonly thumbnail?: {
+    url: string;
+    width: number;
+    height: number;
+  };
 };
