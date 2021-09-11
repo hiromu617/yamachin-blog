32c1052a5b6c6030cfa3702f5e0b0a241ffb71b8
hiro <hiromhiro617@gmail.com>
2021-09-11 21:08:16 +0900

    :pencil2: fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: VFC = () => {
   return (
     <div className="bg-white dark:bg-gray-900">
       <div className="flex justify-center py-5 px-10 items-center border-t-2 border-gray-100 dark:border-gray-700">
-        <p className="text-gray-500 text-xs md:text-md">2021 Hiromu Kawai. All Rights Reserved.</p>
+        <p className="text-gray-500 text-xs md:text-md">2021 <a className="underline" href="https://github.com/hiromu617/yamachin-blog">Hiromu Kawai</a>. All Rights Reserved.</p>
       </div>
     </div>
   );
