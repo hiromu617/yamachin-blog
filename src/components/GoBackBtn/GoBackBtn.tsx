f776cd02188ed0508141091da8ed5be0d6dbe612
hiro <hiromhiro617@gmail.com>
2021-09-15 21:49:06 +0900

    :bug: fix goBackBtn

--- src/components/GoBackBtn/GoBackBtn.tsx
+++ src/components/GoBackBtn/GoBackBtn.tsx
@@ -6,10 +6,18 @@ import { useRouter } from "next/router";
 export const GoBackBtn: VFC = () => {
   const { t } = useLocales();
   const router = useRouter();
+
   return (
     <div className="text-center py-14">
       <button
-        onClick={() => router.back()}
+        onClick={() => {
+          if (typeof window === "undefined") return;
+          if (window.history.length <= 1) {
+            router.push("/");
+          } else {
+            router.back();
+          }
+        }}
         className="text-gray-600 dark:text-gray-200 hover:text-yellow-500 w-auto mx-auto flex gap-1 justify-center items-center"
       >
         <ChevronLeftIcon className=" w-6 h-6" />
