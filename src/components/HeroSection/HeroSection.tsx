7ee6c65893e9db5beab8b96f3e6ac57d4825e155
hiromu617 <hiromhiro617@gmail.com>
2023-01-02 23:08:04 +0900

    fix

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -6,9 +6,9 @@ export const HeroSection: VFC = () => {
   return (
     <div className="mx-auto py-20 md:px-20 text-left md:text-center">
       <h1 className="text-4xl tracking-tight font-extrabold text-gray-700 dark:text-white sm:text-5xl md:text-6xl">
-        India, Causal Inference and
+        Mikuinox
         <br />
-        <span className="text-yellow-500">somtimes Football</span>
+        <span className="text-yellow-500">Football and Data Analytica</span>
       </h1>
       <div className="w-full justify-start md:justify-center my-10 flex gap-2">
         <Scroll to="blogSection" smooth>
